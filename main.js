0d659b26d6a696ca9a30b75687ee3956a8bb03f1
silviacremascoli <silcremascoli@gmail.com>
2023-05-14 12:02:08 +0200

    Added randomNumber and eightBall variables

--- main.js
+++ main.js
@@ -4,4 +4,8 @@ userName === "Jane" ? console.log("Hello, Jane!") : console.log("Hello!");
 
 let userQuestion = "Will I ever be rich?";
 
-console.log(userQuestion);
+console.log(`${userName}: ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8); // gives a random number between 0 and 7
+
+let eightBall = "";
